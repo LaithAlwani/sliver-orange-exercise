3db23b3d3a564630e59f200029ea618604a88f15
Laith Alwani <laithalwani@gmail.com>
2022-05-06 18:09:23 -0400

    fetchs data from github api

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,17 @@
 import { Router, Request, Response } from 'express';
-
+const fetch = require( 'node-fetch');
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-
+  
   res.status(200);
-
+  
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  //geting the data from github api
+  const response = await fetch("https://api.github.com/users/silverorange/repos")
+  const data = await response.json()
+  console.log(data)
+  res.setHeader("content-type","application/json")
+  res.json([...data]);
 });
