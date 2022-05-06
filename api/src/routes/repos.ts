0c0e4cf9e7e1c476a28559fbe0f44e2c3ae216ca
Laith Alwani <laithalwani@gmail.com>
2022-05-06 18:41:47 -0400

    fetchs data from repo.json file and returns aggregate data

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,17 +1,30 @@
 import { Router, Request, Response } from 'express';
-const fetch = require( 'node-fetch');
+import * as fs from 'fs';
+const fetch = require('node-fetch');
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-  
+
   res.status(200);
-  
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  //geting the data from github api
-  const response = await fetch("https://api.github.com/users/silverorange/repos")
-  const data = await response.json()
-  console.log(data)
-  res.setHeader("content-type","application/json")
-  res.json([...data]);
+
+  //fetching data from github api
+  const response = await fetch(
+    'https://api.github.com/users/silverorange/repos'
+  );
+  const data = await response.json();
+
+  //fetching data from repos.json
+  try {
+    const fileData = fs.readFileSync('data/repos.json', 'utf-8');
+    //combinds both datas gahterd
+    const tempData = [...data, ...JSON.parse(fileData)]
+    //returns repositories where `repository.fork` is `false`.
+    const aggergateData = tempData.filter(repo => !repo.fork)
+    
+    res.setHeader('content-type', 'application/json');
+    res.json(aggergateData);
+  } catch (err) {
+    console.log(err);
+  }
 });
