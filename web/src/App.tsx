97d29ef6bf46ea58ad3402b4d853eba97679e574
Laith Alwani <laithalwani@gmail.com>
2022-05-07 13:46:56 -0400

    client side fetchs data from api and sorts it in reverse chronological order by creation date

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-
+import { useEffect, useState } from 'react';
 import './App.css';
 
 export function App() {
+  const [repos, setRepos] = useState([]);
+  //sorts data in reverse chronological order by creation date.
+  const sortData = (data: any) => {
+    const orderedData = data.sort((a: any, b: any) => {
+      if (a.created_at > b.created_at) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+    console.log(orderedData);
+  };
+
+  useEffect(() => {
+    //fetchs data from API
+    const getRepos = () => {
+      fetch('http://localhost:4000/repos')
+        .then((res) => res.json())
+        .then((data) => {
+          sortData(data);
+          setRepos(data);
+        });
+    };
+    if (!repos.length) {
+      getRepos();
+    }
+  }, [repos.length]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {repos ? (
+          repos.map((repo: any) => (
+            <div key={repo.id}>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+              <p>{repo.created_at}</p>
+              <button>{repo.language}</button>
+              <p>forks: {repo.forks_count}</p>
+            </div>
+          ))
+        ) : (
+          <p>no repos found</p>
+        )}
       </header>
     </div>
   );
